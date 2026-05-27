d037757dabfabe189a2d7782a42d67a1d59d44c3
Angel <urielbarrientos358@gmail.com>
2026-05-27 15:46:15 -0600

    mi primer comimit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SistemaEscolar.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
